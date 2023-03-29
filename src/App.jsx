ab7acaed577f5288853c4ccd9c0d9edf6a847d53
André Davedovicz Dettmer <andreddettmer@hotmail.com>
2023-03-29 20:27:10 -0300

    login running

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import SMOBILI from "./img/SMOBILIC.png";
+import TextField from "@mui/material/TextField";
+import Button from '@mui/material/Button';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <img src={SMOBILI} alt="SMOBILI" />
+        <p>Login</p>
+        <TextField id="outlined-basic" label="email" variant="outlined" sx={{width:175,height:175}} />
+        <TextField id="outlined-basic" label="senha" variant="outlined" sx={{width:175,height:175}} />
+        
+        <Button variant="contained" sx={{width:175,height:100,}}>Entrar</Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
 
-export default App
+export default App;
