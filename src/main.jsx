2204130776f561e937b426a236cae5d98728a100
André Davedovicz Dettmer <andreddettmer@hotmail.com>
2023-03-30 17:04:30 -0300

    nada

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import User from "./pages/User";
 import Adm from "./pages/Adm";
-import { BrowserRouter as Router, Routes,Route, Link } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-        <Routes>
-          <Route path="/app" element={<App />} />
-          <Route path="/" element={<User />} />
-          <Route path="/adm" element={<Adm />} />
-        </Routes>
-    </Router>
+    
+    <User/>
   </React.StrictMode>,
 )
