2f2728763b50c5ecf713a45b58e977c407363b46
André Davedovicz Dettmer <andreddettmer@hotmail.com>
2023-03-30 20:06:10 -0300

    fix:router

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import User from "./pages/User";
 import Adm from "./pages/Adm";
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import {BrowserRouter as Router, Routes, Route,Link} from 'react-router-dom';
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    
-    <User/>
-  </React.StrictMode>,
-)
+    <Router>
+            <Routes>
+                <Route path='/' element={<App/>}/>
+                <Route path='/user' element={<User/>}/>
+                <Route path='/adm' element={<Adm/>}/>
+                
+            </Routes>
+        </Router>
+  </React.StrictMode>
+);
